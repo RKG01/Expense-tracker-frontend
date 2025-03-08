e19af9b3384256c4cd2e7a2b6deb51a6b1a0310d
rahul <rk5849193@gmail.com>
2025-03-09 00:25:33 +0530

    add

--- src/App.js
+++ src/App.js
@@ -8,18 +8,25 @@ import IncomeTracker from "./pages/IncomeTracker";
 import Dashboard from "./pages/Dashboard";
 import "./App.css";
 
+// Backend API Base URL (Render Deployment)
+const API_URL = process.env.REACT_APP_BACKEND_URL; 
+
+export const BackendContext = React.createContext(API_URL);
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/tracker" element={<ExpenseTracker />} />
-        <Route path="/income" element={<IncomeTracker />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <BackendContext.Provider value={API_URL}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/tracker" element={<ExpenseTracker />} />
+          <Route path="/income" element={<IncomeTracker />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </BackendContext.Provider>
   );
 }
 
